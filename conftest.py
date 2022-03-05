e78679c2f864b8daf2acfe201de95bd8bae8642d
Ras Al Ghul <smilinazzo@gmail.com>
2022-03-05 16:55:13 -0500

    Smilinazzo/add readme (#3)
    * Add README.md
    
    * Add image_tag option and set ENV vars for no CI runs
    
    * Squash console logging
    
    * Newline
    
    * Add node_version ARG
    
    * Build image if not a CI run
    
    * Add log

--- conftest.py
+++ conftest.py
@@ -1,3 +1,5 @@
+import os
+
 from pathlib import Path
 from _pytest.config.argparsing import Parser
 from src.tools.logger import init_config
@@ -7,16 +9,42 @@ def pytest_addoption(parser: Parser):
     parser.addoption(
         '--artifacts',
         action = 'store',
-        default = '',
+        default = 'src/reports',
         type = Path,
         help = 'Path to store any artifacts that are saved during the test run'
+    )
+    parser.addoption(
+        '--image_tag',
+        action = 'store',
+        default = os.getenv('IMAGE_BASE_TAG'),
+        required = os.getenv('IMAGE_BASE_TAG') is None,
+        help = 'The image_tag to tag the build image'
+    )
+    parser.addoption(
+        '--node_version',
+        action = 'store',
+        default = 'current',
+        help = 'Desired Node Version to build Image'
     )
 
 
 def pytest_configure(config):
+    # Set ENVs
+    if (image := config.getoption('image_tag')) is not None:
+        os.environ['IMAGE_BASE_TAG'] = image
+    if os.getenv('NETWORK') is None:
+        os.environ['NETWORK'] = 'timbernet'
+    if os.getenv('VOLUME') is None:
+        os.environ['VOLUME'] = 'data'
+    if os.getenv('WORKING_DIR') is None:
+        os.environ['WORKING_DIR'] = '/app'
+
+    artifact_dir = config.getoption('artifacts')
+    Path(artifact_dir).mkdir(exist_ok = True)
+
     init_config(
         Path(
-            config.getoption('artifacts'),
+            artifact_dir,
             'timberbrook.log'
         )
     )
